e057ff8bf125739ad9b50eae8d82a41606d5fc0a
Dave Curylo <curylod@asme.org>
2017-09-19 08:07:57 -0400

    Example using with Paket

---
+++ example/using_paket/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+open FSharp.Text.Docker
+open FSharp.Text.Docker.Dockerfile
+
+[<EntryPoint>]
+let main argv =
+    [
+        From ("hello-world", None, None)
+        Expose ([80us; 443us])
+        User ("dave", Some ("greeters"))
+    ]
+    |> Dockerfile.buildDockerfile
+    |> printfn "#### Hello world dockerfile ####%s%s" Environment.NewLine
+    0 // return an integer exit code
